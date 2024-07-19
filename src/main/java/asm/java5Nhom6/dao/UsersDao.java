b86bb9b96971518821a8bae95ae1c68507a4a534
tamXinChao <dominhtam0175@gmail.com>
2024-07-19 12:37:54 +0700

    account

--- src/main/java/asm/java5Nhom6/dao/UsersDao.java
+++ src/main/java/asm/java5Nhom6/dao/UsersDao.java
@@ -12,7 +12,4 @@ import asm.java5Nhom6.entity.Users;
 public interface UsersDao extends JpaRepository<Users, Integer> {
 	  @Query("SELECT u FROM Users u WHERE u.Username = ?1 ")
 	    Users findByUsername(String username);
-	  
-	  
-	  	
 }
